4b76a60eddbc397588b6f2986ae19db8a6d7295d
Samuel Wilson <samuel.wilson@fluxfederation.com>
2018-04-04 21:00:19 +1200

    Iterate over chars to produce tokens

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+extern crate regex;
+
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use regex::Regex;
 
 /// Rusty Ruby
 /// This program is the first stage of a Ruby interpreter. A ruby file is provided as an
@@ -11,8 +14,8 @@ fn main() {
 
     if args.len() == 1 {
         // Exit the program if there are no arguments
-        println!(r###"Usage:
-    rr <ruby-file>"###);
+        println!(r"Usage:
+    rr <ruby-file>");
         std::process::exit(1);
     }
 
@@ -23,4 +26,21 @@ fn main() {
     file.read_to_string(&mut buffer).expect(&format!("Error reading file: {}", filename));
 
     println!("{} contents: {:?}", filename, buffer);
+
+    // These are our individual characters of ruby code, ready for tokenization.
+    let mut chars: Vec<char> = Vec::new();
+    for char in buffer.chars() {
+        chars.push(char);
+    }
+
+    // Creating a numeric matcher with regex
+    let numeric = Regex::new(r"\d|\.").unwrap();
+
+    for char in chars {
+        if numeric.is_match(&char.to_string()) {
+            println!("{} is a numeric.", char);
+        }
+    }
+    // println!("Our vec of chars: {:?}", chars); // chars is moved by the for in loop and no longer available.
+    // We can now make our collection of chars. It's time to iterate over them and tokenize.
 }
